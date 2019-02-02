39fea98bfaf5a9a2a1fdfb327e7c7c29ccf89ebc
deeplyocean <deeplyocean@naver.com>
2019-02-02 15:43:52 +0900

    add test case

--- account/src/test/java/org/ocean/account/AccountDtoTest.java
+++ account/src/test/java/org/ocean/account/AccountDtoTest.java
@@ -1,19 +1,24 @@
 package org.ocean.account;
 
 import org.junit.Test;
+import org.ocean.type.Email;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountDtoTest {
 
     @Test
-    public void accountDtoTest() {
-       AccountDto.Response dto1 = new AccountDto.Response();
-       dto1.setId(0L);
+    public void accountDtoEqualsTest() {
+        AccountDto.Response dto1 = new AccountDto.Response();
+        dto1.setId(0L);
+        dto1.setEmail(Email.builder().value("test@email.com").build());
+        dto1.setAccountName("test");
 
-       AccountDto.Response dto2 = new AccountDto.Response();
-       dto2.setId(0L);
+        AccountDto.Response dto2 = new AccountDto.Response();
+        dto2.setId(0L);
+        dto2.setEmail(Email.builder().value("test@email.com").build());
+        dto2.setAccountName("test");
 
-       assertThat(dto1).isEqualTo(dto2);
+        assertThat(dto1).isEqualTo(dto2);
     }
 }
